0aec43da0eb5b1e7db36ab4a5929bf197c51829d
jaugor7 <sudhansh0706@gmail.com>
2021-05-18 22:07:44 +0530

    Finalizing models

--- textAnalysis/views.py
+++ textAnalysis/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+import pickle
+
+with open('svmModel2.pkl', 'rb') as file:
+    svmModel = pickle.load(file)
+
+with open('tf2.pkl', 'rb') as file:
+    tf = pickle.load(file)
+
 
 # Create your views here.
 
@@ -8,20 +16,26 @@ def index(request):
 
 def giveMePredictionsBudyy(comment):
 
-    ###
+    review_vector = tf.transform([comment])
+    pred =  svmModel.predict(review_vector)
+    print(pred)
 
-    return "Positive"
+    return pred
 
 def predict(request):
     if(request.method == "POST"):
         comment = request.POST.get('myComment')
 
-        prediction = giveMePredictionsBudyy(comment)
-        if(prediction == 'Positive'):
-            messages.success(request, prediction)
-        elif(prediction == 'Neutral'):
-            messages.warning(request, prediction)
-        else:
-            messages.error(request, prediction)
+        pred = giveMePredictionsBudyy(comment)
+        # if(pred[0][0] <= 0.33):
+        #     prediction = "Negative"
+        # elif pred[0][0] <= 0.66:
+        #     prediction = "Neutral"
+        # else:
+        #     prediction = "Positive" 
+        
+        # print(prediction)
+
+        messages.success(request, str(pred))
 
     return redirect('/')
